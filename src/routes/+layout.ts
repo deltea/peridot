c542cf4d65b971952b2d77ed3b505e49387bbcfb
deltea <thcheetah777@gmail.com>
2025-12-14 11:44:24 -0800

    add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,8 @@
 import type { LayoutLoad } from "./$types";
+import { dev } from "$app/environment";
+import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+injectAnalytics({ mode: dev ? "development" : "production" });
 
 export const ssr = false;
 
